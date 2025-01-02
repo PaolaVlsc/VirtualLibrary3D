9c0af25027533f0ab09ff9ce129101e8ef88c230
PaolaVlsc <paolavlsc170698@gmail.com>
2025-01-02 10:02:07 +0200

    Interact with an object to load multiplayer lobby

---
+++ VirtualLibrary/Assets/Scripts/InteractionSystem/LobbyEntrance.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyEntrance : MonoBehaviour, IInteractable
+{
+    // Text to display
+    [SerializeField] private string _interactionPrompt;
+    public string InteractionPrompt => _interactionPrompt;
+
+    public bool Interact(Interactor interactor)
+    {
+        Debug.Log($"Interacting with door: {gameObject.name}");
+        Debug.Log("door Opened");
+        LoadMultiplayerLobby();
+        return true;
+    }
+
+    private void LoadMultiplayerLobby()
+    {
+        SceneManager.LoadScene("MultiplayerLobby");
+    }
+}
